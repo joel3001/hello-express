01b4989f3e29617b3d7b04cb10b99ce11cb20307
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-09-13 18:28:01 +0100

    GET Route test complete

---
+++ __tests__/getShoppingList.test.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const httpMocks = require('node-mocks-http');
+const getShoppingList = require('../controllers/getShoppingList');
+
+it('gets an existing shopping list', (done) => {
+  expect.assertions(1);
+
+  const filename = Date.now().toString();
+
+  const filePath = path.join(__dirname, '../controllers/shoppingLists', filename);
+
+  const body = JSON.stringify({ items: ['carrots', 'crunchies', 'cornflakes'] });
+
+  fs.writeFile(filePath, body, err => {
+    if (err) throw err;
+  });
+
+  const request = httpMocks.createRequest({
+    method: 'GET',
+    url: '/shopping-lists/:filename',
+    params: {
+      filename: filename,
+    },
+  });
+
+  const response = httpMocks.createResponse({
+    eventEmitter: require('events').EventEmitter,
+  });
+
+  getShoppingList(request, response);
+
+  response.on('end', () => {
+    expect(response._getData()).toEqual(body);
+    done();
+  });
+});
